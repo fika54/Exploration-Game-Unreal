f3502effef3e5770f5d1564039d4df7d06de246d
John Ajayi <jajayi@gmail.com>
2025-08-10 16:16:46 +0100

    first commit

---
+++ Source/Attempt1/Attempt1.Build.cs
@@ -0,0 +1,43 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Attempt1 : ModuleRules
+{
+	public Attempt1(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"Attempt1",
+			"Attempt1/Variant_Platforming",
+			"Attempt1/Variant_Combat",
+			"Attempt1/Variant_Combat/AI",
+			"Attempt1/Variant_SideScrolling",
+			"Attempt1/Variant_SideScrolling/Gameplay",
+			"Attempt1/Variant_SideScrolling/AI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/Attempt1/Attempt1GameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Attempt1GameMode.h"
+
+AAttempt1GameMode::AAttempt1GameMode()
+{
+	// stub
+}
